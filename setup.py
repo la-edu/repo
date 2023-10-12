8cfd481522bd3558b7d97d2f876b5adc9a795ef6
Michal Krupa <michalkrupa@gmail.com>
2023-10-11 20:35:31 -0700

    Bugfix: missing comma

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="lacc",
     description="Find Your Path.",
-    long_description="Find Your Path."
+    long_description="Find Your Path.",
     version="1.0",
     packages=find_packages(),
 )
