bf1f3ec10befa7b9243a472eb1be1e020a70e39c
Michal Krupa <michalkrupa@gmail.com>
2023-10-11 20:34:42 -0700

    Update description

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="lacc",
+    description="Find Your Path.",
+    long_description="Find Your Path."
     version="1.0",
     packages=find_packages(),
 )
